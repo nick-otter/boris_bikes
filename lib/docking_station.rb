3a7fcc51d730535fed819f8b7ed45416ccc102aa
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-02 14:34:14 +0100

    Challenge 11: Completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,20 @@
 $LOAD_PATH << '.'
 
 class DockingStation
-  class Bike
-    def working?
-      true
+    class Bike
+        def working?
+            true
+        end
     end
-  end
+
+  attr_reader :bike
+
   def release_bike
     bike = Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,10 +4,15 @@ describe DockingStation do
   describe "release bike" do
     it { is_expected.to respond_to(:release_bike) }
   end
-  describe DockingStation do
-    bike = Bike.new
-      it "bike is working" do
-        expect(bike.working?).to be true
+describe DockingStation do
+  bike = Bike.new
+    it "bike is working" do
+      expect(bike.working?).to be true
   end
+end
+describe DockingStation do
+  describe "docks bike" do
+    it { is_expected.to respond_to(:dock).with(1).argument }
   end
+end
 end
